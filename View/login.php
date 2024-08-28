3896c195e50002841fdb48b4cc112fdbf80eae2e
lito-gut <kathygutierrez42@gmail.com>
2024-08-28 13:16:43 -0600

    Login
    Cambios esteticos en login

--- View/login.php
+++ View/login.php
@@ -6,35 +6,17 @@
 <head>
                 <title>ComidiTicas</title>
                 <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
                 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
                 <link rel="stylesheet" href="dist/css/adminlte.min.css">
                 <link rel="stylesheet" href="dist/css/styles.css">
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
-                <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap4.css">
-                <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
-                <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
-                <link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet">
-                <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-                <link rel="stylesheet" href="css/animate.css">
-                <link rel="stylesheet" href="css/owl.carousel.min.css">
-                <link rel="stylesheet" href="css/owl.theme.default.min.css">
-                <link rel="stylesheet" href="css/magnific-popup.css">					
-                <link rel="stylesheet" href="css/aos.css"
-                <link rel="stylesheet" href="css/ionicons.min.css">
-                <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-                <link rel="stylesheet" href="css/jquery.timepicker.css"
-                <link rel="stylesheet" href="css/flaticon.css">
-                <link rel="stylesheet" href="css/icomoon.css">
-                <link rel="stylesheet" href="css/style.css">
             </head>
 
-<body  class="hold-transition login-page bg-dark">
+<body  class="hold-transition login-page bg-dark" style="background-image: url(/images/bg_1.jpg);">
 <div class="login-box" >
   <div class="login-logo">
-    <a style= "color: #fff;">Comidi
+  <img src="/images/CR.jpg" alt="CR" class="mr-1" >
+    <a href="../../index.php"><b style= "color: #fff;">Comidi
             <br>
             <small style= "color: #fac564;">Ticas</small>
           </a>
--- View/recuperar.php
+++ View/recuperar.php
@@ -1,27 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
   <title>ComidiTicas| Recuperar</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Font Awesome -->
+  <meta charset="utf-8">
   <link rel="stylesheet" href= "plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- icheck bootstrap -->
   <link rel="stylesheet" href= "../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   </head>
-<body  class="hold-transition login-page bg-dark"  >
+<body  class="hold-transition login-page bg-dark" style="background-image: url(/images/bg_1.jpg);" >
 
 <div class="login-box" >
   <div class="login-logo">
+  <img src="/images/CR.jpg" alt="CR" class="mr-1" >
     <a href="../../index.php"><b style= "color: #fff;">Comidi
             <br>
             <small style= "color: #fac564;">Ticas</small>
